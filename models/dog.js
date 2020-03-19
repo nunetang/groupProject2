8e5d9588d692361523a1a6fade58579b5e8be265
nicolaeghetan <nicoale.ghetan@gmail.com>
2020-03-18 21:21:04 -0700

    added  model for user and dogs

---
+++ models/dog.js
@@ -0,0 +1,58 @@
+//Creating our Dog Model
+module.exports = function (sequelize, DataTypes) {
+    var Dog = sequelize.define("Dog", {
+        //Name of the  dog  cannot be null and   only  from latters restricted  on  limit of characters
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    args: true,
+                    msg: "Required"
+                },
+                is: {
+                    args: ["^[a-z]+$", "i"],
+                    msg: "Only letters allowed"
+                },
+                len: {
+                    args: [1, 100],
+                    msg: "Name of the  dog is not in this range"
+                }
+            }
+        },
+        // Age of  our   dog also cannot  be null and restricted   by lenght
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    args: true,
+                    msg: "Age of the dog is required"
+                },
+                isInt: true,
+                len: [1, 25]
+            }
+        },
+        gender: {
+            type: DataTypes.STRING
+        },
+        color: {
+            type: DataTypes.STRING,
+            longFur: DataTypes.BOOLEAN
+        },
+        discription: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        type: {
+            type: DataTypes.STRING,
+            specifications: DataTypes.STRING,
+            validate: {
+                isString: true,
+                len: [1, 250],
+                notEmpty: true
+            }
+        }
+    });
+    return Dog;
+};
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 var bcrypt = require("bcryptjs");
 // Creating our User model
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // The email cannot be null, and must be a proper email before creation
     email: {
@@ -16,15 +16,31 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    name: {
+      type: DataType.STRING,
+      validate: {
+        notEmpty: true,
+        is: ["^[a-z]+$", "i"],
+        len: [1, 100]
+      }
+    },
+    address: {
+      type: DataType.STRING,
+      allowNull: true,
+      validate: {
+        notEmpty: true,
+        is: /\b\d{1,6} +.{2,25}\b(avenue|ave|court|ct|street|st|drive|dr|lane|ln|road|rd|blvd|plaza|parkway|pkwy)[.,]?(.{0,25} +\b\d{5}\b)?/gi
+      }
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", function(user) {
+  User.addHook("beforeCreate", function (user) {
     user.password = bcrypt.hashSync(
       user.password,
       bcrypt.genSaltSync(10),
