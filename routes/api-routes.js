c0a36131d51dd5857e9754eb0aa9713fc4fb0c8e
nicolaeghetan <nicoale.ghetan@gmail.com>
2020-03-20 20:20:26 -0700

    Fixed api_rout files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,11 +2,11 @@
 var db = require("../models");
 var passport = require("../config/passport");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function (req, res) {
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
     // Sending back a password, even a hashed password, isn't a good idea
     res.json({
       email: req.user.email,
@@ -17,27 +17,27 @@ module.exports = function (app) {
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function (req, res) {
+  app.post("/api/signup", function(req, res) {
     db.User.create({
       email: req.body.email,
       password: req.body.password
     })
-      .then(function () {
+      .then(function() {
         res.redirect(307, "/api/login");
       })
-      .catch(function (err) {
+      .catch(function(err) {
         res.status(401).json(err);
       });
   });
 
   // Route for logging user out
-  app.get("/logout", function (req, res) {
+  app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
   });
 
   // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function (req, res) {
+  app.get("/api/user_data", function(req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
@@ -50,43 +50,47 @@ module.exports = function (app) {
       });
     }
   });
-  app.post("/api/user", (req, res) => {
+  app.post("/api/user", function(res, req) {
     db.User.create({
       name: req.body.name,
       address: req.body.address
-    }).then(function (result) {
+    }).then(function(result) {
       res.json(result);
     });
   });
-  app.get("/api/user", (req, res) => {
-    db.User.findAll({})
-      .then(function (result) {
-        res.json(result);
-      })
+  app.get("/api/user", function(res) {
+    db.User.findAll({}).then(function(result) {
+      res.json(result);
+    });
   });
-  app.delete("/api/user/:id", (req, res) => {
+  app.delete("/api/user/:id", function(res) {
     db.User.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function (result) {
+    }).then(function(result) {
       res.json(result);
     });
   });
-  app.get("/api/user/:id", (req, res) => {
+  app.get("/api/user/:id", function(res) {
     db.User.findAll({
       where: {
         id: req.params.id
       }
-    }).then(function (result) {
+    }).then(function(result) {
       res.json(result);
     });
   });
-  app.post("/api/dog", (res, req) => {
+  app.post("/api/dog", function(res, req) {
     db.Dog.create({
-
-    })
-  })
-
-
+      name: req.body.name,
+      age: req.body.age,
+      gender: req.body.gender,
+      color: req.body.color,
+      longFur: req.body.longFur,
+      discription: req.body.discription,
+      hyper: req.body.hyper,
+      patient: req.body.patient
+    });
+  });
 };
