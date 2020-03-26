1e1245a417300d4cb8511efb763907e68226704e
Impactful Actions <eturner.et3@outlook.com>
2020-03-25 21:43:08 -0700

    Add dog functionality working. Updated profile.html, profile.js, and dog-api-routes.js to resolve bug. Server.js updated to include dog and owner api routes. db seeds updated to include zipcode. Dog.js model updated to match info in profile.html and profile.js. CURRENT STATUS: Authentication successful, redirected to profile page, select Add Dog, input dog info, Submit dog info, sent to DB via POST. Modal closed automatically.

--- routes/dog-api-routes.js
+++ routes/dog-api-routes.js
@@ -20,10 +20,27 @@ module.exports = function(app) {
     });
   });
 
-  app.post("api/dogs", function(req, res) {
-    db.Dog.create(req.body).then(function(dbDog) {
-      res.json(dbDog);
-    });
+  app.post("/api/create/dog", function(req, res) {
+    db.Dog.create({
+      name: req.body.name,
+      age: req.body.age,
+      gender: req.body.gender,
+      weight: req.body.weight,
+      bio: req.body.bio,
+      energetic: req.body.energetic,
+      social: req.body.social,
+      aggressive: req.body.aggressive,
+      color: req.body.color,
+      longFur: req.body.longFur,
+      type: req.body.type
+    })
+      .then(function(result) {
+        // res.redirect(307, "/profile");
+        res.json(result);
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
   });
 
   app.delete("/api/dogs/:id", function(req, res) {
